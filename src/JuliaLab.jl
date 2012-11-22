119dfaae8af5c3e470dbe527bec2168a9ca4b6e6
autozimu <autozimu@gmail.com>
2012-11-21 22:16:11 -0500

    Change string manipulate style

--- src/JuliaLab.jl
+++ src/JuliaLab.jl
@@ -12,16 +12,17 @@ _PLOTPOINTS_ = 100
 function to_str(xa::Array)
     str = "["
     for x in xa
-        str = strcat(str, string(x), ", ")
+        str = "$str$x, "
     end
-    str = strcat(str, "]")
+    str = "$str]"
     return str
 end
 
 ## run matplotlib commands, to adjust figure ditail, like ticks
 ## TODO: support block parameters
 function mrun(cmd::String)
-    cmd = strcat(server, " \'", cmd, "\'")
+    ## single quote cmd to avoid confusing system call, ie, shell
+    cmd = "$server \'$cmd\'"
     #println(cmd)
     system(cmd)
 end
@@ -36,8 +37,7 @@ function figure()
     mrun("figure()")
 end
 function figure(num::Integer)
-    cmd = strcat("figure(", num, ")")
-    mrun(cmd)
+    mrun("figure($num)")
 end
 fig = figure
 
@@ -46,8 +46,7 @@ function showfig()
     mrun("show()")
 end
 function showfig(num::Integer)
-    cmd = strcat("show(", num, ")")
-    mrun(cmd)
+    mrun("show($num)")
 end
 
 ## clear figure
@@ -60,71 +59,64 @@ function closefig()
     mrun("close()")
 end
 function closefig(num::Integer)
-    cmd = strcat("close(", num, ")")
-    mrun(cmd)
+    mrun("close($num)")
 end
 
 ## save figure
-function savefig(s::String)
-    cmd = strcat("savefig(\"", s, "\")")
-    mrun(cmd)
+function savefig(file::String)
+    mrun("savefig($file)")
 end
 
 ## main plot function
 function plot(x::Array, y::Array, args::Tuple)
-    # try convert to float
+    # convert to float
 
     # check array dimension
     if ndims(x) != 1 || ndims(y) != 1
-        println("SyntaxError: parameters should be one dimension array!")
+        println("SyntaxError: input arrays should be of one dimension!")
         return
+    # check length of arrays
+    elseif size(x, 1) != size(y, 1)
+        println("SyntaxError: lengths of input arrays should be equal!")
     end
-
     # check number of arguments
     if rem(length(args), 2) == 1
-        println("SyntaxError: Symbols and parameters not pair!")
+        println("SyntaxError: Symbols and parameters should in pair!")
         return
     end
 
+    cmd = "plot("
     # translate x, y
-    if length(x) == 0
-        cmdea = ""
-    else
-        cmdea = strcat(to_str(x), ", ")
+    if length(x) != 0
+        cmd = "$cmd$(to_str(x)), "
     end
-    if length(y) == 0
-        cmdeb = ""
-    else
-        cmdeb = strcat(to_str(y), ", ")
-    end
-    cmd = strcat("plot(", cmdea, cmdeb)
+    cmd = "$cmd$(to_str(y)), "
 
     # translate arguments
     for i = 1:2:length(args)
-        if isa(args[i], Symbol) == false
-            println("SyntaxError: failed when retriving Symbol")
+        if !isa(args[i], Symbol)
+            println("SyntaxError: args should use Symbol!")
             return
         end
 
         if isa(args[i+1], String)
-            cmde = strcat(args[i], "=\"", args[i+1], "\"")
+            cmd = "$cmd $(args[i])=\"$(args[i+1])\", "
         else
-            cmde = strcat(args[i], "=", args[i+1])
+            cmd = "$cmd $(args[i])=$(args[i+1]), "
         end
-        cmd = strcat(cmd, cmde, ", ")
     end
 
-    cmd = strcat(cmd, ")")
+    cmd = "$cmd)"
     mrun(cmd)
 end
 
 ## plot x, y
 ## syntax: plot(x, y, :option, parameters)
 plot(x::Array, y::Array, args...) = plot(x, y, args)
-## plot y
+## plot y, real or complex
 function plot(cx::Array, args...)
     if typeof(cx[1]) <: Real
-        plot([], cx, args)
+        plot([1:length(cx)], cx, args)
     elseif typeof(cx[1]) <: Complex
         x = Array(Float64, size(cx, 1))
         y = Array(Float64, size(cx, 1))
@@ -143,87 +135,76 @@ function plot(f::Function, xmin::Real, xmax::Real, args...)
         plot(x, y, args)
 end
 
-## plot file
+## plotfile
 function plotfile(f::String, args::Tuple)
     # check number of arguments
     if rem(length(args), 2) == 1
-        println("SyntaxError: Symbols and parameters not pair")
+        println("SyntaxError: Symbols and parameters should in pair!")
         return
     end
 
-    cmd = strcat("plotfile(\"", f, "\"", ", ")
-
+    cmd = "plotfile(\"$f\", "
     # translate arguments
     for i = 1:2:length(args)
         if isa(args[i], Symbol) == false
-            println("SyntaxError: Failed to retrive Symbol")
+            println("SyntaxError: args should use Symbol!")
             return
         end
 
         if isa(args[i+1], String)
-            cmde = strcat(args[i], "=\"", args[i+1], "\"")
+            cmd = "$cmd $(args[i])=\"$(args[i+1])\", "
         else
-            cmde = strcat(args[i], "=", args[i+1])
+            cmd = "$cmd $(args[i])=$(args[i+1]), "
         end
-        cmd = strcat(cmd, cmde, ", ")
     end
 
-    cmd = strcat(cmd, ")")
+    cmd = "$cmd)"
     mrun(cmd)
 end
 plotfile(f::String, args...) = plotfile(f, args)
 
 
 ## set xlim
-function xlim(xmin::Number, xmax::Number)
-    cmd = strcat("xlim(", xmin, ", ", xmax, ")")
-    mrun(cmd)
+function xlim(xmin::Real, xmax::Real)
+    mrun("xlim($xmin, $xmax)")
 end
 
 ## set ylim
-function ylim(ymin::Number, ymax::Number)
-    cmd = strcat("ylim(", ymin, ", ", ymax, ")")
-    mrun(cmd)
+function ylim(ymin::Real, ymax::Real)
+    mrun("ylim($ymin, $ymax)")
 end
 
 ## set title
 function title(s::String)
-    cmd = strcat("title(\"", s, "\")")
-    mrun(cmd)
+    mrun("title(\"$s\")")
 end
 
 ## set xlabel
 function xlabel(s::String)
-    cmd = strcat("xlabel(\"", s, "\")")
-    mrun(cmd)
+    mrun("xlabel(\"$s\")")
 end
 
 ## set ylabel
 function ylabel(s::String)
-    cmd = strcat("ylabel(\"", s, "\")")
-    mrun(cmd)
+    mrun("ylabel(\"$s\")")
 end
 
 ## legend
-function legend(labels, loc)
-    if isa(labels, Tuple) == false
-        println("SyntaxError: labels should be tuple!")
-        return
-    end
-
+function legend(labels::Tuple, loc::String)
     if length(labels) == 0
-        cmd = strcat("legend(", "loc = \"", loc, "\")")
+        cmd = "legend(\"$loc\")"
     else
-        cmde = "("
+        cmd = "("
         for label in labels
-            cmde = strcat(cmde, "\"", label, "\", ")
+            cmd = "$cmd\"$label\", "
         end
-        cmde = strcat(cmde, ")")
-        cmd = strcat("legend(", cmde, ", loc=\"", loc, "\")")
+        cmd = "$cmd)"
+        cmd = "legend($cmd, loc=\"$loc\""
     end
+    cmd = "$cmd)"
     mrun(cmd)
 end
-legend(loc) = legend((), loc)
+legend(loc::String) = legend((), loc)
 legend() = legend((), "")
 
 ## test
@@ -238,12 +219,13 @@ function test()
     ylabel(E"$y$")
     legend((E"$sin(x)$", ), "upper left")
 
-    plot(x, y, :linestyle, "None", :marker, "o", :color, "r")
+    plot(x, y, :linestyle, "None", :marker, "o", :color, "r", :linewidth, 2)
 
-    cx = [-2.0 + 0.0im, 0.0 + 0.0im, 2.0 + 0.0im]
+    cx = [-2.0 + 1.0im, 0.0 + 0.0im, 2.0 + -1.0im]
     plot(cx)
 
     #plotfile("test.dat")
+    #plotfile("test.dat", :linewidth, 2, :linestyle, "None")
 end
 
 end # end module
