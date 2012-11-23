cb1e67e800bca3083db8aafbbe10bc5b4f5affd8
autozimu <autozimu@gmail.com>
2012-11-23 18:42:36 -0500

    Change env variable

--- src/setup.sh
+++ src/setup.sh
@@ -4,7 +4,7 @@
 # Description: start tmux session for julia
 # Created: November 17, 2012
 
-export JuliaLab_HOME="~/Documents/Projects/JuliaLab.jl/src"
+export JuliaLab_HOME="$HOME/Documents/Projects/JuliaLab.jl/src"
 
 SESSION_NAME="IJulia"
 
@@ -13,7 +13,7 @@ if [ "$?" -eq 1 ]; then
     echo "No session found. Creating new one ..."
     tmux new-session -d -s $SESSION_NAME
     tmux send-keys "ipython qtconsole --pylab \
-        > $JuliaLab_HOME/kernel.info && clear" C-m
+        > $JuliaLab_HOME/kernel.info; clear &" C-m
     tmux split-window -v 'julia'
     tmux send-keys "require(\"JuliaLab\")" C-m
 else
