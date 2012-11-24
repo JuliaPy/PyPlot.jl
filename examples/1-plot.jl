c7b34f3875108870bc2e2b5732d6c16baad317b1
autozimu <autozimu@gmail.com>
2012-11-24 01:34:42 -0500

    Add 2 examples

---
+++ examples/1-plot.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+# File: 1-plot.jl
+# Author: Junfeng Li <li424@mcmaster.ca>
+# Description: simple plot
+# Created: November 24, 2012
+
+require("JuliaLab")
+using JuliaLab
+
+x = linspace(-pi, pi)
+plot(x, sin(x), :label, E"$sin(x)$")
+xlim(-2pi, 2pi)     # set xrange
+ylim(-2, 2)         # set yrange
+title(E"$sin(x)$")
+xlabel(E"$x$")
+ylabel(E"$y$")
+legend()            # show legend
+grid(true)              # show grid
+
+#savefig("1-plot.pdf")
---
+++ examples/2-plot.jl
@@ -0,0 +1,34 @@
+#!/usr/bin/env julia
+# File: 2-plot.jl
+# Author: Junfeng Li <li424@mcmaster.ca>
+# Description: subplot
+# Created: November 24, 2012
+
+require("JuliaLab")
+using JuliaLab
+
+x = linspace(-pi, pi, 20)
+y = sin(x)
+cx = [-1 + 0im, 0 + 1im, 1 + 0im]
+
+subplot(221)
+title("Plot without lines")
+grid(true)
+plot(x, y, :linestyle, "None", :marker, "o")    # don't draw lines
+
+subplot(222)
+title("Plot one array")
+grid(true)
+plot(y)             # plot one array
+
+subplot(223)
+title("Plot complex array")
+grid(true)
+plot(cx)            # plot complex array
+
+subplot(224)
+title("Plot function")
+grid(true)
+plot(cos, -pi, pi)  # plot a function
+
+#savefig("2-plot.pdf")
