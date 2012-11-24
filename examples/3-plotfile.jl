11a91d84493bfc6834ad8d58cba5d3d00e5f7fec
autozimu <autozimu@gmail.com>
2012-11-24 01:52:39 -0500

    Add plotfile example

---
+++ examples/3-plotfile.jl
@@ -0,0 +1,13 @@
+#!/usr/bin/env julia
+# File: 3-plotfile.jl
+# Author: Junfeng Li <li424@mcmaster.ca>
+# Description: plot file
+# Created: November 24, 2012
+
+require("JuliaLab")
+using JuliaLab
+
+plotfile("3-plotfile.txt", :delimiter, " ", :cols, (0, 1), :marker, "^")
+title("Plot data from file")
+
+savefig("3-plotfile.pdf")
