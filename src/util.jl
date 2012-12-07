aadffa19be7d760114d29b5afeed591d796f4715
autozimu <autozimu@gmail.com>
2012-12-06 20:58:53 -0500

    Remove file util.jl:

--- src/util.jl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env julia
-# File: util.jl
-# Author: Junfeng Li <li424@mcmaster.ca>
-# Description: useful functions that hasn't been implemented by julia core
-# team yet
-# Created: November 21, 2012
