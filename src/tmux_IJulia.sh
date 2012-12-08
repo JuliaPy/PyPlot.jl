b9f14ec96424c4d63fcf2a6f6aae37e5c55a69d5
autozimu <autozimu@gmail.com>
2012-12-08 01:54:07 -0500

    Make daemonize ipython a little better

--- src/tmux_IJulia.sh
+++ src/tmux_IJulia.sh
@@ -11,8 +11,7 @@ tmux has-session -t $SESSION_NAME 2>/dev/null
 if [ "$?" -eq 1 ]; then
     echo "No session found. Creating new one ..."
     tmux new-session -d -s $SESSION_NAME
-    tmux send-keys "ipython kernel --pylab &>$PYPLOT_JL_HOME/kernel.info &" C-m
-    tmux send-keys "sleep 5 && bg && clear" C-m
+    tmux send-keys "ipython kernel --pylab <&- &>$PYPLOT_JL_HOME/kernel.info &" C-m
     tmux split-window -v
     tmux send-keys "julia" C-m
 else
