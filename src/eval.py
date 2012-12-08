447119b6f62288a7e29728d40ff57dcc42d646bc
autozimu <autozimu@gmail.com>
2012-12-08 04:09:07 -0500

    Continue simplify eval code

--- src/eval.py
+++ src/eval.py
@@ -5,7 +5,7 @@
 # Created: November 20, 2012
 
 ## FIXME
-# The most suitable solution for this scenario is starting this script
+# The most suitable solution for this scenario may be starting this script
 # as a subprocess and send plot command directly to its STDIN. But I
 # cannot find enouth information about open and communicate with a
 # persistant subprocess in Julia, and I cann't implement it in Python,
@@ -16,22 +16,11 @@
 # * https://github.com/ivanov/vim-ipython
 # * https://github.com/ipython/ipython/blob/master/IPython/frontend/terminal/console/interactiveshell.py
 
+from sys import argv
 from IPython.lib.kernel import find_connection_file
 from IPython.zmq.blockingkernelmanager import BlockingKernelManager
 
-# evalutate code
-def run_cell(code):
-    # now we can run code. This is done on the shell channel
-    shell = km.shell_channel
-    # execution is immediate and async, returning a UUID
-    msg_id = shell.execute(code)
-    # get_meg can block for a reply
-    reply = shell.get_msg()
-
-    if reply['content']['status'] == 'error':
-        for line in reply['content']['traceback']:
-            print line
-
+# startup channel
 pidfile = '/tmp/pyplot-jl-ipython-daemon.pid'
 with open(pidfile, 'r') as f:
     cf = f.readline()
@@ -43,6 +32,14 @@ km.connection_file = cf
 km.load_connection_file()
 km.start_channels()
 
+# parse commands
+code = argv[1]
 
-from sys import argv
-run_cell(argv[1])
+# execution is immediate and async, returning a UUID
+msg_id = km.shell_channel.execute(code)
+# get_meg can block for a reply
+reply = km.shell_channel.get_msg()
+
+if reply['content']['status'] == 'error':
+    for line in reply['content']['traceback']:
+        print line
