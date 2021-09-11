f659d9b2613baadd846a3c0fd9b05fb635578969
Stanislav Kazmin <998651+stakaz@users.noreply.github.com>
2021-09-11 23:27:45 +0200

    changed to proposed solution with quote "subplot"

--- src/plot3d.jl
+++ src/plot3d.jl
@@ -58,7 +58,7 @@ for f in mplot3d_funcs
     fs = string(f)
     @eval @doc LazyHelp(axes3D,"Axes3D", $fs) function $f(args...; kws...)
         using3D() # make sure mplot3d is loaded
-					PyPlot.version <= v"3.4" ? ax = gca(projection="3d") : ax = plt.subplot(projection="3d")
+					ax = PyPlot.version <= v"3.4" ? gca(projection="3d") : plt."subplot"(projection="3d")
         pycall(ax.$fs, PyAny, args...; kws...)
     end
 end
@@ -74,7 +74,7 @@ for f in zlabel_funcs
     fs = string("set_", f)
     @eval @doc LazyHelp(axes3D,"Axes3D", $fs) function $f(args...; kws...)
         using3D() # make sure mplot3d is loaded
-          PyPlot.version <= v"3.4" ? ax = gca(projection="3d") : ax = plt.subplot(projection="3d")
+					ax = PyPlot.version <= v"3.4" ? gca(projection="3d") : plt."subplot"(projection="3d")
         pycall(ax.$fs, PyAny, args...; kws...)
     end
 end
