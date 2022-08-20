0873d740c939acfc5da695d18000fea719b8d0e7
Juri Linkov <jurta@jurta.org>
2022-08-20 16:44:29 +0300

    Fix typo in colormaps.jl (#517) (#528)

--- src/colormaps.jl
+++ src/colormaps.jl
@@ -173,7 +173,7 @@ function show(io::IO, ::MIME"image/svg+xml", cs::AbstractVector{ColorMap})
     pad = 0.5
     write(io,
         """
-        <?xml version"1.0" encoding="UTF-8"?>
+        <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
          "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
